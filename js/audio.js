bb944b52231d3a964947dcebf7f5142bb25db191
venmorr <stevenmorrrca@email.com>
2023-09-05 09:07:37 -0400

    test if three wav files is to much for git

--- js/audio.js
+++ js/audio.js
@@ -1,17 +1,14 @@
 /*----------Variables-----------*/
 // let b1Audio = new Audio('assets/audio/sc1.wav')
-// let b2Audio = new Audio('assets/audio/sc2.wav')
+let swordAudioII = new Audio('assets/audio/sc2.wav')
 let swordAudioIIIAudio = new Audio('assets/audio/sc3.wav')
 let swordAudioIV = new Audio('assets/audio/sc4.wav')
 /*----------Functions-----------*/
-// function playB1Audio() {
-//   b1Audio.volume = 0.25
-//   b1Audio.play()
-// }
-// function playB2Audio() {
-//   b2Audio.volume = 0.25
-//   b2Audio.play()
-// }
+
+function playSwordAudioII() {
+  swordAudioII.volume = 0.25
+  swprdAudioII.play()
+}
 function playSwordAudioIII() {
   swordAudioIII.volume = 0.25
   swprdAudioIII.play()
@@ -23,8 +20,7 @@ function playSwordAudioIV() {
 
 /*------------Export------------*/
 export {
-  // playB1Audio,
-  // playB2Audio,
+  playSwordAudioII,
   playSwordAudioIII,
   playSwordAudioIV
 }
